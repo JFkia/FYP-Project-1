7089439fe05e75a704c9ed9674ac687070521d55
JFKia <23016399@myrp.edu.sg>
2025-11-06 14:15:36 +0800

    small changes

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-new application.js
+const express = require('express');
+const flash = require('connect-flash');
+const session = require('express-session');
+const app = express();
+// backend/server.js
+const express      = require('express');
+const cors         = require('cors');
+const mongoose     = require('mongoose');
+require('dotenv').config();
+
