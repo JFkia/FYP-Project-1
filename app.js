18ba30c305e5a6490164fd9e538fc23d59805560
JFKia <23016399@myrp.edu.sg>
2025-11-17 21:04:34 +0800

    updated app.js

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
 const express = require('express');
-const flash = require('connect-flash');
 const session = require('express-session');
-const app = express();
-// backend/server.js
-const express      = require('express');
-const cors         = require('cors');
-const mongoose     = require('mongoose');
-require('dotenv').config();
+const flash = require('connect-flash');
+const fs = require('fs');
+const path = require('path');
 
+const app = express();
+
+app.use(session({
+    secret: 'secret123',
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(flash());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// AUTO LOAD ALL ROUTE HANDLERS IN /views
+fs.readdirSync(path.join(__dirname, './views'))
+  .forEach((file) => {
+      if (file.endsWith('.js')) {
+          require(`./views/${file}`)(app);
+      }
+  });
+
+module.exports = app;
